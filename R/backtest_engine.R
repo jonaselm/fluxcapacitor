5dcb31b874e4d556ded6f123183f23f2a19d0b1e
jonaselm <jonaselm@me.com>
2018-02-13 10:36:31 -0500

    Added xts conversion helper function.
    add_indicator function created
    
    Updated DESCRIPTION file.

--- R/backtest_engine.R
+++ R/backtest_engine.R
@@ -1,3 +1,25 @@
 # This file includes the main backtest engine code for fluxcapacitor.
 #
 #
+
+#' add_indicator
+#'
+#' @param security an object of type tibble contanining security data
+#' @param indicator_name the desired column name of the indicator, used to identify signals
+#' @param generator_fn the function used to generate the indicator value
+#' @param generator_args a named list of parameters to use with the generator function
+#'
+#' @return a tibble of security data with the indicator added as a new column
+#' @export
+#'
+#' @examples
+add_indicator <- function(security, indicator_name, generator_fn, generator_args){
+
+  if(!is_tibble(security)) stop("Price data must be in Tibble format.")
+
+  security[indicator_name] <- do.call(generator_fn, generator_args)
+
+  return(security)
+
+}
+
---
+++ R/data_formatting.R
@@ -0,0 +1,3 @@
+# Wrapper function to convert an xts dataset to a tibble with a Date column
+xts_to_tibble <- function(xts_object) xts_object %>% timetk::tk_tbl(preserve_index = TRUE, rename_index = "Date")
+
