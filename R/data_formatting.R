923febe1c4b28cd13c2ef70336de07faa95165d7
jonaselm <jonaselm@me.com>
2018-03-06 15:07:52 -0500

    Bug fixes

--- R/data_formatting.R
+++ R/data_formatting.R
@@ -1,16 +1,27 @@
 #Add ticker to stock data object
-add_ticker <- function(x){
+add_ticker <- function(x, ticker){
   ticker <- as.character(match.call()$x)
-  x$"Ticker" <- ticker
+  x$Ticker <- ticker
   return(x)
 }
 
 # Wrapper function to convert an xts dataset to a tibble with a Date column
-xts_to_tibble <- function(xts_object) xts_object %>% add_ticker %>% timetk::tk_tbl(preserve_index = TRUE, rename_index = "Date")
+xts_to_tibble <- function(xts_object) suppressWarnings(xts_object %>% timetk::tk_tbl(preserve_index = TRUE, rename_index = "Date"))
 
 
 print.fc_strategy <- function(strat_object){
 
   paste("A fluxcapacitor strategy backtest object with", length(strat_object$Data), "securities", sep = " ")
 
+}
+
+build_universe <- function(ticker){
+  #Make sure data is in a usable format
+  if(!tibble::is_tibble(get(universe[ticker])) & !xts::is.xts(get(universe[ticker]))) stop("Price data must be in Tibble format.")
+
+  #Convert any xts data to tibble w/ date column
+  if(xts::is.xts(get(universe[ticker]))) xts_to_tibble(get(universe[ticker]))
+
+  #Otherwise, simply get tibble
+  if(tibble::tibble(get(universe[ticker]))) get(universe[ticker])
 }
