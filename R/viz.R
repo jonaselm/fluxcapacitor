c283fa32634168b9b2386ebf4921462986382be4
jonaselm <jonaselm@me.com>
2018-03-12 09:04:53 -0400

    Added labels and theme to chart_equity_curve()

--- R/viz.R
+++ R/viz.R
@@ -1,6 +1,7 @@
 chart_equity_curve <- function(strategy_object){
 
-  strategy_object$ledger %>% ggplot2::ggplot(aes(x = Date, y = Acct_Val)) + geom_line()
+  strategy_object$ledger %>% ggplot2::ggplot(aes(x = Date, y = Acct_Val)) + geom_line() +
+    labs(x = "Date", y = "Equity Curve", title = paste("Equity Curve for", substitute(strategy_object), sep = " ")) + theme_bw()
 
 }
 
