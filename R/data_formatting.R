a026b2b0b2f3c682fc149f2a9d65d569b8dfd9e8
jonaselm <jonaselm@me.com>
2018-03-13 11:32:04 -0400

    Added helper functions to deal with missing dates.

--- R/data_formatting.R
+++ R/data_formatting.R
@@ -1,27 +1,22 @@
-#Add ticker to stock data object
-add_ticker <- function(x, ticker){
-  ticker <- as.character(match.call()$x)
-  x$Ticker <- ticker
-  return(x)
-}
-
 # Wrapper function to convert an xts dataset to a tibble with a Date column
 xts_to_tibble <- function(xts_object) suppressWarnings(xts_object %>% timetk::tk_tbl(preserve_index = TRUE, rename_index = "Date"))
 
-
-print.fc_strategy <- function(strat_object){
+print.fc_strategy <- function(strat_object) {
 
   paste("A fluxcapacitor strategy backtest object with", length(strat_object$Data), "securities", sep = " ")
 
 }
 
-build_universe <- function(ticker){
-  #Make sure data is in a usable format
-  if(!tibble::is_tibble(get(universe[ticker])) & !xts::is.xts(get(universe[ticker]))) stop("Price data must be in Tibble format.")
+# Internal helper function to deal with empty numerics resulting from calls to subset dates that don't exist
+if_filled <- function(x) {
 
-  #Convert any xts data to tibble w/ date column
-  if(xts::is.xts(get(universe[ticker]))) xts_to_tibble(get(universe[ticker]))
+  ifelse(length(x) > 0, x, 0)
 
-  #Otherwise, simply get tibble
-  if(tibble::tibble(get(universe[ticker]))) get(universe[ticker])
+}
+
+# Internal helper function to find index of nearest filled date in position data
+nearest_date <- function(x, i_date){
+
+  as.numeric(x %>% filter(Date <= i_date) %>% select(Date) %>% summarize(Last = n()))
+
 }
