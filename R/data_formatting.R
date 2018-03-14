d4d6f2c3cef95362fb03421af99ce2a6bba8cde3
jonaselm <jonaselm@me.com>
2018-03-14 17:35:32 -0400

    Removed deprecated functions.

--- R/data_formatting.R
+++ R/data_formatting.R
@@ -1,22 +1,3 @@
 # Wrapper function to convert an xts dataset to a tibble with a Date column
 xts_to_tibble <- function(xts_object) suppressWarnings(xts_object %>% timetk::tk_tbl(preserve_index = TRUE, rename_index = "Date"))
 
-print.fc_strategy <- function(strat_object) {
-
-  paste("A fluxcapacitor strategy backtest object with", length(strat_object$Data), "securities", sep = " ")
-
-}
-
-# Internal helper function to deal with empty numerics resulting from calls to subset dates that don't exist
-if_filled <- function(x) {
-
-  ifelse(length(x) > 0, x, 0)
-
-}
-
-# Internal helper function to find index of nearest filled date in position data
-nearest_date <- function(x, i_date){
-
-  as.numeric(x %>% filter(Date <= i_date) %>% select(Date) %>% summarize(Last = n()))
-
-}
